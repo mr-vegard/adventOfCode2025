09a7f91d3f90949c90562d873ffbb098fcdbaa78
Vegard Vik <Vegardvik97@gmail.com>
2025-12-03 20:50:54 +0100

    day 1 part 1

---
+++ day1/input_dial.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from typing import Literal
+
+input_list: list[str] = []
+
+with open("input.txt", "r") as f:
+    input_list = f.readlines()
+
+
+@dataclass
+class DialInput():
+    direction: Literal["L", "R"]
+    value: int
+
+dial_inputs: list[DialInput] = []
+
+for input in input_list:
+    dial_inputs.append(DialInput(direction=input[:1], value=int(input[1:])))
+
+
+def dialer(dial_current: int, direction: Literal["L", "R"], dial_input: int):
+    for i in range(dial_input):
+        del i
+
+        if direction == "R":
+            if dial_current == 99:
+                dial_current -= 100
+            dial_current += 1
+
+        if direction == "L":
+            if dial_current == 0:
+                dial_current += 100
+            dial_current -= 1
+
+    return dial_current
+
+zero_counter = 0
+dial_current: int = 50
+
+for dial_input in dial_inputs:
+    dial_current = dialer(dial_current, dial_input.direction, dial_input=dial_input.value)
+    if dial_current == 0:
+        zero_counter += 1
+
+    print(dial_current)
+print(zero_counter)

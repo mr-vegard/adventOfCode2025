9937c32704391527144e5a3148b8b426b9bb9ef7
Vegard Vik <Vegardvik97@gmail.com>
2025-12-03 20:58:08 +0100

    day 1 part 2

--- day1/input_dial.py
+++ day1/input_dial.py
@@ -18,9 +18,12 @@ for input in input_list:
     dial_inputs.append(DialInput(direction=input[:1], value=int(input[1:])))
 
 
-def dialer(dial_current: int, direction: Literal["L", "R"], dial_input: int):
+def dialer(dial_current: int, zero_counter_current, direction: Literal["L", "R"], dial_input: int):
     for i in range(dial_input):
         del i
+
+        if dial_current == 0:
+            zero_counter_current += 1
 
         if direction == "R":
             if dial_current == 99:
@@ -32,15 +35,13 @@ def dialer(dial_current: int, direction: Literal["L", "R"], dial_input: int):
                 dial_current += 100
             dial_current -= 1
 
-    return dial_current
+    return dial_current, zero_counter_current
 
-zero_counter = 0
 dial_current: int = 50
+zero_counter = 0
 
 for dial_input in dial_inputs:
-    dial_current = dialer(dial_current, dial_input.direction, dial_input=dial_input.value)
-    if dial_current == 0:
-        zero_counter += 1
+    dial_current, zero_counter = dialer(dial_current, zero_counter, dial_input.direction, dial_input=dial_input.value)
 
     print(dial_current)
 print(zero_counter)

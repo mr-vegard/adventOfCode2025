9f30354bb7d25787cb1fbd4c4ff144a9dfc3640f
Vegard Vik <Vegardvik97@gmail.com>
2025-12-03 23:00:46 +0100

    day 3 part 1

---
+++ day3/joltage_bank_calculator.py
@@ -0,0 +1,30 @@
+input_list: list[str] = []
+
+with open("input.txt", "r") as f:
+    input_list = f.readlines()
+
+print(input_list)
+
+def find_largest_two_numbers(number: int):
+    number_as_string = str(number)
+    biggest_first_number = 0
+    for i in range(0, len(number_as_string)-1):
+        if int(number_as_string[i]) > biggest_first_number:
+            biggest_first_number = int(number_as_string[i])
+        if biggest_first_number == 9:
+            break
+
+    biggest_first_number_position = number_as_string.index(str(biggest_first_number)) + 1
+    
+    biggest_second_number = 0
+    for i in range(biggest_first_number_position, len(number_as_string)):
+        if int(number_as_string[i]) > biggest_second_number:
+            biggest_second_number = int(number_as_string[i])
+    
+    return int(str(biggest_first_number)+str(biggest_second_number))
+
+number_total = 0
+
+for number in input_list:
+    number_total += find_largest_two_numbers(int(number.strip()))
+    print(number_total)
